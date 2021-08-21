7afd694d53416601b158ee7488cfd16aad056b25
Artem Pasichnyk <palladin.93@hotmail.com>
2021-08-21 18:22:46 +0200

    adds testing

--- go.mod
+++ go.mod
@@ -6,4 +6,6 @@ require (
 	github.com/gin-gonic/gin v1.7.4 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/huandu/go-sqlbuilder v1.12.2 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/vektra/mockery/v2 v2.9.0 // indirect
 )
---
+++ internal/platform/server/handler/courses/create_test.go
@@ -0,0 +1,72 @@
+package courses
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/artemidas/hexagonal-http-api/internal/platform/storage/storagemocks"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+const (
+	endpoint = "/course"
+)
+
+func TestHandler_Create(t *testing.T) {
+	courseRepository := new(storagemocks.CourseRepository)
+	courseRepository.
+		On("Save", mock.Anything, mock.AnythingOfType("mooc.Course")).
+		Return(nil)
+
+	gin.SetMode(gin.TestMode)
+	r := gin.New()
+	r.POST(endpoint, CreateHandler(courseRepository))
+
+	t.Run("given an invalid request it returns 400", func(t *testing.T) {
+		createCourseReq := createRequest{
+			ID:   "c542a87a-1d70-4cdc-9cb6-f93fbb9a70bd",
+			Name: "Demo Course",
+		}
+
+		b, err := json.Marshal(createCourseReq)
+		require.NoError(t, err)
+
+		req, err := http.NewRequest(http.MethodPost, endpoint, bytes.NewBuffer(b))
+		require.NoError(t, err)
+
+		rec := httptest.NewRecorder()
+		r.ServeHTTP(rec, req)
+
+		res := rec.Result()
+		defer res.Body.Close()
+
+		assert.Equal(t, http.StatusBadRequest, res.StatusCode)
+	})
+
+	t.Run("given a valid request it returns 201", func(t *testing.T) {
+		createCourseReq := createRequest{
+			ID:       "c542a87a-1d70-4cdc-9cb6-f93fbb9a70bd",
+			Name:     "Demo Course",
+			Duration: "10 months",
+		}
+
+		b, err := json.Marshal(createCourseReq)
+		require.NoError(t, err)
+
+		req, err := http.NewRequest(http.MethodPost, endpoint, bytes.NewBuffer(b))
+		require.NoError(t, err)
+
+		rec := httptest.NewRecorder()
+		r.ServeHTTP(rec, req)
+
+		res := rec.Result()
+		defer res.Body.Close()
+
+		assert.Equal(t, http.StatusCreated, res.StatusCode)
+	})
+}
