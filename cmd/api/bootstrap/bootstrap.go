d259e3573db4b0ae58a8709e84370f9ea1b8fd65
Artem Pasichnyk <palladin.93@hotmail.com>
2021-09-06 22:39:33 +0200

    adds environment variables

--- cmd/api/bootstrap/bootstrap.go
+++ cmd/api/bootstrap/bootstrap.go
@@ -11,24 +11,18 @@ import (
 	"github.com/artemidas/hexagonal-http-api/internal/platform/server"
 	"github.com/artemidas/hexagonal-http-api/internal/platform/storage/mysql"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/kelseyhightower/envconfig"
 	"time"
 )
 
-const (
-	host            = "localhost"
-	port            = 8080
-	shutdownTimeout = 10 * time.Second
-
-	dbUser    = "root"
-	dbPass    = "password"
-	dbHost    = "localhost"
-	dbPort    = "3306"
-	dbName    = "codely_courses"
-	dbTimeout = 5 * time.Second
-)
-
 func Run() error {
-	mysqlURI := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPass, dbHost, dbPort, dbName)
+	var cfg config
+	err := envconfig.Process("MOOC", &cfg)
+	if err != nil {
+		return err
+	}
+
+	mysqlURI := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", cfg.DbUser, cfg.DbPass, cfg.DbHost, cfg.DbPort, cfg.DbName)
 	db, err := sql.Open("mysql", mysqlURI)
 	if err != nil {
 		return err
@@ -38,7 +32,7 @@ func Run() error {
 		commandBus = inmemory.NewCommandBus()
 		eventBus   = inmemory.NewEventBus()
 	)
-	courseRepository := mysql.NewCourseRepository(db, dbTimeout)
+	courseRepository := mysql.NewCourseRepository(db, cfg.DbTimeout)
 
 	creatingCourseService := creating.NewCourseService(courseRepository, eventBus)
 	increasingCourseService := increasing.NewCourseCounterService()
@@ -52,6 +46,20 @@ func Run() error {
 		creating.NewIncreaseCoursesCounterOnCourseCreated(increasingCourseService),
 	)
 
-	ctx, srv := server.New(context.Background(), host, port, shutdownTimeout, commandBus)
+	ctx, srv := server.New(context.Background(), cfg.Host, cfg.Port, cfg.ShutdownTimeout, commandBus)
 	return srv.Run(ctx)
+}
+
+type config struct {
+	// Server configuration
+	Host            string        `default:"localhost"`
+	Port            uint          `default:"8080"`
+	ShutdownTimeout time.Duration `default:"10s"`
+	// Database configuration
+	DbUser    string        `default:"codely"`
+	DbPass    string        `default:"codely"`
+	DbHost    string        `default:"localhost"`
+	DbPort    uint          `default:"3306"`
+	DbName    string        `default:"codely"`
+	DbTimeout time.Duration `default:"5s"`
 }
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/huandu/go-sqlbuilder v1.12.2 // indirect
+	github.com/kelseyhightower/envconfig v1.4.0 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/vektra/mockery/v2 v2.9.0 // indirect
 )
